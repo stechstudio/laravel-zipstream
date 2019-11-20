af6a99a84f70daead19052661071b67c7e3038f6
Joseph <joseph@stechstudio.com>
2019-11-20 13:25:10 -0500

    typo

--- tests/SizeEstimation/SizeEstimationTestCase.php
+++ tests/SizeEstimation/SizeEstimationTestCase.php
@@ -46,7 +46,7 @@ class SizeEstimationTestCase extends TestCase
 
     public function assertSizeEstimation()
     {
-        $estimate = $this->zip->predictZipDataSize();
+        $estimate = $this->zip->predictZipSize();
 
         $this->zip->saveTo("/tmp");
 
