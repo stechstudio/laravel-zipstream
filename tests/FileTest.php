4fd5e1d8b8023f718a65e28328c3946b71da6f86
Joseph Szobody <joseph@stechstudio.com>
2020-02-10 23:03:55 -0500

    check for windows path format

--- tests/FileTest.php
+++ tests/FileTest.php
@@ -15,6 +15,7 @@ class FileTest extends TestCase
         $this->assertInstanceOf(S3File::class, File::make('s3://bucket/key'));
         $this->assertInstanceOf(LocalFile::class, File::make('/dev/null'));
         $this->assertInstanceOf(LocalFile::class, File::make('/tmp/foobar'));
+        $this->assertInstanceOf(LocalFile::class, File::make('C:/foo/bar'));
         $this->assertInstanceOf(TempFile::class, File::make("raw contents", "filename.txt"));
     }
 
