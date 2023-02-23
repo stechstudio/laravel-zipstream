c4f511615a9ff63934f21916c4eaedc7c8adb310
Joseph Szobody <joseph@stechstudio.com>
2023-02-23 15:57:15 -0500

    Flip the order of this check. Resolves #96

--- src/ZipStream.php
+++ src/ZipStream.php
@@ -317,8 +317,8 @@ class ZipStream extends BaseZipStream implements Responsable
      */
     public function canPredictZipSize()
     {
-        return $this->queue->every->canPredictZipDataSize()
-            && config('zipstream.archive.predict');
+        return config('zipstream.archive.predict')
+            && $this->queue->every->canPredictZipDataSize();
     }
 
     /**
